3c4c9e96fe33e827843ff17f2e86c7da0870f021
379675019 <yyzclc@163.com>
2019-07-22 16:11:27 +0800

    Update liff-starter.js

--- liff-starter.js
+++ liff-starter.js
@@ -1,10 +1,11 @@
 window.onload = function (e) {
     liff.init(function (data) {
+        alert(data.context.userId)
         var url = 'http://3.16.164.193:9080/api/line/add-device?userId=' + data.context.userId;
         liff.openWindow({
             url: url
         });
-        liff.closeWindow();
+        // liff.closeWindow();
     });
 };
 
